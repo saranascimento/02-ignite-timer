77a45785924331a509d746388e0ad6b734d3db55
Sara Gomes <sarah.gomes80@hotmail.com>
2023-02-16 09:43:22 -0300

    Estrutura da aplicação - Cores & fonte

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,9 +11,9 @@ export const GlobalStyle = createGlobalStyle`
     outline: none;
     box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
   }
-  
+
   body {
-   background: ${(props) => props.theme["gray-900"]};
+    background: ${(props) => props.theme["gray-900"]};
     color: ${(props) => props.theme["gray-300"]};
   }
 
