282f555f32750602b7e6f44051c7c1f7efb8adc6
Sara Gomes <sarah.gomes80@hotmail.com>
2023-02-21 21:38:18 -0300

    Contexto no React - Entendendo contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { ThemeProvider } from 'styled-components'
-import { Router } from './Router'
-import { BrowserRouter } from 'react-router-dom'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+// import { ThemeProvider } from 'styled-components'
+// import { Router } from './Router'
+// import { BrowserRouter } from 'react-router-dom'
+// import { GlobalStyle } from './styles/global'
+// import { defaultTheme } from './styles/themes/default'
+import { Home } from './Home'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+  //     <GlobalStyle />
+  //   </ThemeProvider>
+  // )
+
+  return <Home />
 }
