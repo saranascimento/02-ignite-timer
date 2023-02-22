5719574fe4bba0aba308ceb9e5f9e12f394285f7
Sara Gomes <sarah.gomes80@hotmail.com>
2023-02-21 22:11:30 -0300

    Contexto no React - Convertendo para contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-// import { ThemeProvider } from 'styled-components'
-// import { Router } from './Router'
-// import { BrowserRouter } from 'react-router-dom'
-// import { GlobalStyle } from './styles/global'
-// import { defaultTheme } from './styles/themes/default'
-import { Home } from './Home'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //     </BrowserRouter>
-  //     <GlobalStyle />
-  //   </ThemeProvider>
-  // )
-
-  return <Home />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
